be88a1729fe2e7c355b96fa15aa2c63e2c6bc848
Daniel Martinez <danny102271@sbcglobal.net>
2018-04-24 08:40:27 -0600

    Snap Challenge

---
+++ reddit.php
@@ -0,0 +1,18 @@
+<?php
+
+class reddit{
+	public function _construct($text){
+		$this->text($text);
+	}
+public function text($text){
+	}echo $text;
+}
+}
+$example= new Example('Some Text');
+?>
+/**
+ * Created by PhpStorm.
+ * User: Daniel Martinez
+ * Date: 4/24/2018
+ * Time: 8:16 AM
+ */
